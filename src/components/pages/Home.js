b1c196e9e63364b6c311a72f8147ab4aa0f5cf45
Keshav Jaiswal <keshav.jaiswal39@gmail.com>
2021-05-28 00:20:09 +0530

    Created the Footer Components

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,15 +1,18 @@
+  
 import React from 'react';
 import '../../App.css';
 import Cards from '../Cards';
 import HeroSection from '../HeroSection';
+import Footer from '../Footer';
 
-function Home () {
-    return (
-        <>
-            <HeroSection />
-            <Cards/>
-        </>
-    )
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Cards />
+      <Footer />
+    </>
+  );
 }
 
 export default Home;
