47703b6bad43869e1261cdeb1d89c10d24a1f17b
Keshav Jaiswal <keshav.jaiswal39@gmail.com>
2021-05-27 23:02:13 +0530

    Created the card components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
 import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 
 function App() {
@@ -12,6 +15,9 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home}/>
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
         </Switch>
       </Router>
     </>
