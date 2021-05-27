995f2846a8575b1310c9a182e3e8189a16577351
Keshav Jaiswal <keshav.jaiswal39@gmail.com>
2021-05-28 00:52:50 +0530

    Project Completed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -22,7 +22,7 @@ function HeroSection() {
           buttonStyle='btn--primary'
           buttonSize='btn--large'
           onClick={console.log('hey')}
-          >
+        >
           WATCH TRAILER <i className='far fa-play-circle' />
         </Button>
       </div>
